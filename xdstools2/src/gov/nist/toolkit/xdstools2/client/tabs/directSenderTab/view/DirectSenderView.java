585cc0f4425f7cb2d8b43adc8a09ba43a6600317
bill <devnull@localhost>
2012-11-20 21:00:34 -0500

    start of mvp pattern usage

---
+++ xdstools2/src/gov/nist/toolkit/xdstools2/client/tabs/directSenderTab/view/DirectSenderView.java
@@ -0,0 +1,316 @@
+package gov.nist.toolkit.xdstools2.client.tabs.directSenderTab.view;
+
+import gov.nist.toolkit.results.client.AssertionResult;
+import gov.nist.toolkit.results.client.Result;
+import gov.nist.toolkit.xdstools2.client.PopupMessage;
+import gov.nist.toolkit.xdstools2.client.tabs.directSenderTab.DirectSenderTab;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+
+public class DirectSenderView implements DirectSenderTab.Display {
+	TextBox directServerNameTextBox = new TextBox();
+	TextBox directFromAddressTextBox = new TextBox();
+	TextBox directToAddressTextBox = new TextBox();
+	ListBox messageSelectionListBox = new ListBox();
+	VerticalPanel topPanel;
+	VerticalPanel resultPanel;
+	Button submitButton = new Button();
+	FileUpload upload = null;
+	FormPanel form = null;
+	VerticalPanel vpan = new VerticalPanel();
+	boolean submitComplete = true;  // needed to manage a race condition
+
+	static String CHOOSE = "-- Choose --";
+	
+	String textBoxWidth = "25em";
+	String listBoxWidth = "25em";
+
+
+	public DirectSenderView(VerticalPanel topPanel) {
+		this.topPanel = topPanel;
+		this.resultPanel = new VerticalPanel();
+	}
+	
+	public void build() {
+		
+		HTML title = new HTML();
+		title.setHTML("<h2>Send Direct Message</h2>");
+		topPanel.add(title);
+
+		directServerNameTextBox.setWidth(textBoxWidth);
+		directServerNameTextBox.setText("");
+
+		directFromAddressTextBox.setWidth(textBoxWidth);
+
+		directToAddressTextBox.setWidth(textBoxWidth);
+
+		messageSelectionListBox.setWidth(listBoxWidth);
+		
+		FlexTable mainGrid = new FlexTable();
+
+		int row = 0;
+		mainGrid.setWidget(row, 0, new HTML("Direct System"));
+		mainGrid.setWidget(row, 1, new HTML("Server Name"));
+		mainGrid.setWidget(row, 2, directServerNameTextBox);
+		mainGrid.setWidget(row, 3, new HTML("Direct system to send message to on port 25"));
+
+		row++;
+		mainGrid.setWidget(row, 0, new HTML("Direct"));
+		mainGrid.setWidget(row, 1, new HTML("From Address"));
+		mainGrid.setWidget(row, 2, directFromAddressTextBox);
+		mainGrid.setWidget(row, 3, new HTML("From: field of message header"));
+
+		row++;
+		mainGrid.setWidget(row, 1, new HTML("To Address"));
+		mainGrid.setWidget(row, 2, directToAddressTextBox);
+		mainGrid.setWidget(row, 3, new HTML("To: field of message header"));
+
+		row++;
+
+		row++;
+		mainGrid.setWidget(row, 0, new HTML("Message Selection"));
+		mainGrid.setWidget(row, 2, messageSelectionListBox);
+		mainGrid.setWidget(row, 3, new HTML("Docment to be sent as attachment"));
+		row++;
+		
+		HorizontalPanel formatPanel = new HorizontalPanel();
+		List<RadioButton> wrappedSelection = new ArrayList<RadioButton>();
+		RadioButton wrappedRadio = new RadioButton("Wrapped");
+		wrappedRadio.setText("Wrapped");
+		wrappedRadio.setEnabled(false);
+		wrappedSelection.add(wrappedRadio);
+
+		RadioButton unwrappedRadio = new RadioButton("Unwrapped");
+		unwrappedRadio.setText("Unwrapped");
+		unwrappedRadio.setValue(true);
+		wrappedSelection.add(unwrappedRadio);
+		mainGrid.setWidget(row, 0, new HTML("Message format"));
+		formatPanel.add(unwrappedRadio);
+		formatPanel.add(wrappedRadio);
+		mainGrid.setWidget(row, 2, formatPanel);
+		
+		topPanel.add(mainGrid);
+		
+		topPanel.add(new HTML("<br /><hr /><br />"));
+		topPanel.add(new HTML("<h3>Certificates to be used</h3>"));
+
+		addCertUpload(false, 
+				"Signing Cert",
+				"This cert will be used to sign the message. It must be in PKCS12 format and include the private key. The password field should be left blank if the key is not password protected. " +
+				"The OU (organizational unit) of this cert must match the domain name of the From Address entered above. This assumes a domain level cert.", 
+				"1", 
+				true);
+		
+		addCertUpload(true, 
+				"Encryption Cert", 
+				"This cert is the public key of the receiver that will be used to encrypt the message. It must be in DER format.", 
+				"2", 
+				false);
+
+	}
+
+	void addCertUpload(boolean submit, String label, String doc, String fieldSuffix, boolean showPassword) {
+		//
+		// Upload signing cert
+		//
+		
+		if (form == null) {
+			form = new FormPanel();
+			form.setAction(GWT.getModuleBaseURL() + "upload");
+			form.setEncoding(FormPanel.ENCODING_MULTIPART);
+			form.setMethod(FormPanel.METHOD_POST);
+		}
+		
+		HorizontalPanel hpan = new HorizontalPanel();
+		vpan.add(hpan);
+		form.setWidget(vpan);
+		
+		hpan.add(new HTML(label));
+		upload = new FileUpload();
+		upload.setName("upload" + fieldSuffix + "FormElement");
+		hpan.add(upload);
+		
+		if (showPassword) {
+			hpan.add(new HTML("Password"));
+			TextBox tb = new TextBox();
+			tb.setName("password" + fieldSuffix);
+			hpan.add(tb);
+		}
+		
+		if (submit) {
+			vpan.add(new HTML("<br /><hr /><br />"));
+			vpan.add(new Button("Submit", new ClickHandler() {
+				public void onClick(ClickEvent event) {
+					form.submit();
+				}
+			}));
+		}
+		hpan.add(new HTML(doc));
+		
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+			// fired just before the form is submitted.  Validation
+			// can be done here. The submit can be canceled via
+			// event.cancel();
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				if (!checkInputs()) 
+					event.cancel();
+				else
+					submitComplete = false;
+			}
+		});
+		form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+			// When the form submission is successfully completed, this event
+			// is fired.
+			@Override
+			public void onSubmitComplete(SubmitCompleteEvent event) {
+				if (submitComplete)
+					return;
+				submitComplete = true;
+				Map<String, String> parms = new HashMap<String, String>();
+				clearResultPanel();
+
+				if (!checkInputs()) 
+					return;
+				
+				parms.put("$direct_server_name$", display.getServerName());
+				parms.put("$direct_from_address$", display.getFromAddress());
+				parms.put("$direct_to_address$", display.getToAddress());
+				parms.put("$ccda_attachment_file$", display.getSelectedMessageName());
+
+//				addStatusBox();
+				try {
+					getGoButton().setVisible(false);
+				} catch (Exception e) {}
+				if (getInspectButton() != null)
+					getInspectButton().setEnabled(false);
+
+				toolkitService.directSend(parms, new AsyncCallback<List<Result>> () {
+					public void onFailure(Throwable caught) {
+						display.clearResultPanel();
+						display.addHTMLToResultPanel("<font color=\"#FF0000\">" + "Error running validation: " + caught.getMessage() + "</font>");
+					}
+
+					public void onSuccess(List<Result> results) {
+						boolean status = true;
+						for (Result result : results) {
+							for (AssertionResult ar : result.assertions.assertions) {
+								String assertion = ar.assertion;
+								if (assertion != null)
+									assertion = assertion.replaceAll("\n", "<br />");
+								if (ar.status) {
+									display.addHTMLToResultPanel(assertion);
+								} else {
+									if (assertion.contains("EnvironmentNotSelectedException"))
+										display.addHTMLToResultPanel("<font color=\"#FF0000\">" + "Environment Not Selected" + "</font>");
+									else
+										display.addHTMLToResultPanel("<font color=\"#FF0000\">" + assertion + "</font>");
+									status = false;
+								}
+							}
+						}
+						if (status)
+							setStatus("Status: Success", true);
+						else
+							setStatus("Status: Failure", false);
+						//getGoButton().setEnabled(true);
+					}});
+			}
+		});
+		
+		vpan.add(new HTML("<br />"));
+				
+		topPanel.add(form);
+		
+	}
+
+	@Override
+	public void setMessageSelections(List<String> names) {
+		messageSelectionListBox.clear();
+		messageSelectionListBox.addItem(CHOOSE);
+		for (String name : names)
+			messageSelectionListBox.addItem(name);
+	}
+
+	@Override
+	public String getServerName() {
+		return directServerNameTextBox.getText();
+	}
+
+	@Override
+	public String getFromAddress() {
+		return directFromAddressTextBox.getText();
+	}
+
+	@Override
+	public String getToAddress() {
+		return directToAddressTextBox.getText();
+	}
+
+	@Override
+	public String getSelectedMessageName() {
+		int selected = messageSelectionListBox.getSelectedIndex();
+		if (selected == -1 || selected == 0)
+			return "";
+		return messageSelectionListBox.getItemText(selected);
+	}
+
+	@Override
+	public void addHTMLToResultPanel(String html) {
+		resultPanel.add(new HTML(html));
+	}
+
+	@Override
+	public void clearResultPanel() {
+		resultPanel.clear();
+	}
+
+	boolean checkInputs() {
+		String errMsg = checkInputs2();
+		if (errMsg == null)
+			return true;
+		new PopupMessage(errMsg);
+		return false;
+	
+	}
+	String checkInputs2() {
+		if ("".equals(getServerName())) {
+			return "Server Name is required";
+		}
+		if ("".equals(getFromAddress())) {
+			return "From Address is required";
+		}
+		if ("".equals(getToAddress())) {
+			return "To Address is required";
+		}
+		String selected = getSelectedMessageName();
+		if (selected.equals("")) {
+			return "Message Selection is required";
+		}
+		String fn = upload.getFilename();
+		if (fn == null || fn.equals("")) {
+			return "Signing Cert is required";
+		}
+		return null;
+	}
+}
