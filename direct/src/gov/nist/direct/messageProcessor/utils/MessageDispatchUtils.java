5fb8520af37bad668e14740a9d32daa30f840cbe
julienperugini <devnull@localhost>
2013-02-15 13:17:48 -0500

    Solved MDN validation bug

--- direct/src/gov/nist/direct/messageProcessor/utils/MessageDispatchUtils.java
+++ direct/src/gov/nist/direct/messageProcessor/utils/MessageDispatchUtils.java
@@ -1,6 +1,9 @@
 package gov.nist.direct.messageProcessor.utils;
 
+import java.io.IOException;
+
 import gov.nist.direct.messageProcessor.direct.directImpl.DirectMimeMessageProcessor;
+import gov.nist.direct.messageProcessor.direct.directImpl.WrappedMessageProcessor;
 import gov.nist.direct.utils.ParseUtils;
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 
@@ -8,6 +11,7 @@ import javax.mail.MessagingException;
 import javax.mail.Part;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 public class MessageDispatchUtils {
@@ -31,9 +35,10 @@ public class MessageDispatchUtils {
 	 * false if it is not.
 	 * @throws MessagingException 
 	 */
-	public static boolean isMDN (ErrorRecorder er, MimeMessage msg) throws MessagingException{
-		if (msg.getContentType().contains("multipart/report") ||
-				msg.getContentType().contains("message//disposition-notification")) {
+	public static boolean isMDN (ErrorRecorder er, byte[] msg, byte[] directCertificate, String password) throws MessagingException{
+		WrappedMessageProcessor processor = new WrappedMessageProcessor();
+		processor.messageParser(er, msg , directCertificate, password);
+		if (processor.getIsMDN()) {
 			return true;
 		}
 				return false;	
