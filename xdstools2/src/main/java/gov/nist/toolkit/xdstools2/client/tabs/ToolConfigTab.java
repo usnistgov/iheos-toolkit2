edddd67796f04cd296ef31b818e74dfb3d8a830f
dazais <devnull@localhost>
2015-11-11 22:45:58 +0100

    Tidy up Toolkit Configuration tab

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/ToolConfigTab.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/ToolConfigTab.java
@@ -27,7 +27,7 @@ public class ToolConfigTab extends GenericQueryTab {
 	FlexTable grid = new FlexTable();
 	Map<String, String> props;
 	Button rmOldSims = new Button("Delete old simulators");
-	Button loadAllGazelleConfigs = new Button("Load all Gazelle Configs");
+	Button loadAllGazelleConfigs = new Button("Load all Gazelle configs");
 	int gridRow;
 
 	public ToolConfigTab() {
@@ -45,12 +45,8 @@ public class ToolConfigTab extends GenericQueryTab {
 		HTML title = new HTML();
 		title.setHTML("<h2>Configure XDS Toolkit</h2>");
 		topPanel.add(title);
-		
-		topPanel.add(addHTML("<h3>Properties</h3>"));
-		
+
 		buildGrid();
-		
-		
 	}
 	
 	class RmOldSimsClickHandler implements ClickHandler {
@@ -85,44 +81,46 @@ public class ToolConfigTab extends GenericQueryTab {
 	};
 
 
-	
+
 	void buildGrid() {
-		
+
 		if (PasswordManagement.isSignedIn) {
 		}
 		else {
 			PasswordManagement.addSignInCallback(signedInCallback);
-			
+
 			new AdminPasswordDialogBox(topPanel);
-			
+
 			return;
-			
 		}
 
-		
 		loadPropertyFile();
 
-		
+		HTML subtitle1 = new HTML();
+		subtitle1.setHTML("<h3>Properties</h3>");
+		topPanel.add(subtitle1);
+
+		topPanel.add(grid);
+
 		Button goButton = new Button("Save");
 		goButton.addClickHandler(new Saver());
-		
-		topPanel.add(grid);
-		
-		topPanel.add(goButton);	
-		
+		topPanel.add(goButton);
+
 		HTML separator = new HTML();
-		separator.setHTML("<hl />");
+		separator.setHTML("<br/>");
 		topPanel.add(separator);
-		
+
+		HTML subtitle2 = new HTML();
+		subtitle2.setHTML("<h3>Cleanup & Refresh</h3>");
+		topPanel.add(subtitle2);
+
 		topPanel.add(rmOldSims);
 		rmOldSims.addClickHandler(new RmOldSimsClickHandler());
-		
+
 		topPanel.add(loadAllGazelleConfigs);
 		loadAllGazelleConfigs.addClickHandler(new LoadGazelleConfigsClickHandler(toolkitService, myContainer, "ALL"));
-		
-
 	}
-	
+
 	class Saver implements ClickHandler {
 
 		public void onClick(ClickEvent event) {
@@ -196,17 +194,27 @@ public class ToolConfigTab extends GenericQueryTab {
 		grid.setWidget(gridRow, 1, w);
 		gridRow++;
 	}
-	
+
+	/**
+	 * Build the grid of toolkit properties for display. The property names (keys) will be correctly formatted for
+	 * display here as long as underscores are used in the toolkit properties file.
+	 */
 	void loadPropertyGrid() {
 		grid.clear();
 		gridRow = 0;
 		for (String key : props.keySet()) {
-			String value = props.get(key);
-			grid.setText(gridRow, 0, key);
+
+			// create the label for each row
+			String formattedKey = key.replace('_', ' ');
+			grid.setText(gridRow, 0, formattedKey);
+
+			// create the boxed value for each row
 			TextBox tb = new TextBox();
 			tb.setWidth("600px");
+			String value = props.get(key);
 			tb.setText(value);
 			grid.setWidget(gridRow, 1, tb);
+
 			gridRow++;
 		}
 	}
