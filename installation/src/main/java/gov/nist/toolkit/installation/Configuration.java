943b692eff3e830a45506da36bc74fd79340fb38
bill <devnull@localhost>
2016-02-03 06:44:09 -0500

    Add config.properties file to control internal operation/options.  Class Configuration is used to query.

---
+++ installation/src/main/java/gov/nist/toolkit/installation/Configuration.java
@@ -0,0 +1,30 @@
+package gov.nist.toolkit.installation;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Manage configuration properties.
+ * These control the services available and are not generally user
+ * editable.
+ */
+public class Configuration {
+    static Logger logger = Logger.getLogger(Configuration.class);
+    static Properties properties = new Properties();
+
+    static {
+        InputStream is = Configuration.class.getResourceAsStream("/config.properties");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            logger.info("Cannot load config.properties");
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
