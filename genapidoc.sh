fb9aa2441646f68547a382a1e7996209e8cd0efc
bill <devnull@localhost>
2016-02-16 11:09:29 -0500

    Fix IT - XDR send.  List and Map used in resources can only reference Resources (not interfaces) .  Had to expose DocumentResource.

--- genapidoc.sh
+++ genapidoc.sh
@@ -8,6 +8,7 @@ mkdir -p $output
 
 javadoc -d $output \
 `find toolkit-api -name '*.java'  -not -name '*XdsDocumentRegRep.java' -not -name '*XcaInitiatingGateway.java' -not -name '*XcaRespondingGateway.java' -not -name '*XdsDocumentConsumer.java'` \
-`find toolkit-services-common -name '*.java' -not -name '*Resource.java' -not -name '*Adapter.java'`
+`find toolkit-services-common -name '*.java' -not -name '*Resource.java' -not -name '*Adapter.java'` \
+`find toolkit-services-common -name DocumentResource`
 
 
