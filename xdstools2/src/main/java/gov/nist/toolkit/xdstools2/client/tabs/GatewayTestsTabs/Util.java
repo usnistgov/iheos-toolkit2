ec2ff1e9fbe6884dda7a13113a9e96a68fe1f4f4
bill <devnull@localhost>
2016-02-25 11:56:01 -0500

    Add hander for readme.txt files coded in HTML.

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/GatewayTestsTabs/Util.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/GatewayTestsTabs/Util.java
@@ -15,10 +15,25 @@ public class Util {
     }
 
     public static String htmlize(String in) {
+        if (isHtml(in))
+            return allButFirstLine(in);
         return
                 in.replaceAll("<", "&lt;")
                         .replaceAll("\t", "&nbsp;&nbsp;&nbsp;")
                         .replaceAll(" ", "&nbsp;")
                         .replaceAll("\n", "<br />");
+    }
+
+    static boolean isHtml(String x) {
+        return x.contains("<p>") ||
+                x.contains("<h2>") ||
+                x.contains("<br");
+    }
+
+    static String allButFirstLine(String x) {
+        int eol = x.indexOf('\n');
+        if (eol > 0)
+            return x.substring(eol+1);
+        return x;
     }
 }
