f0f0a8c42e9c929c0ca989993cb1f62da27dd868
bill <devnull@localhost>
2016-03-03 08:08:46 -0500

    API documentation updates.

--- genapidoc.sh
+++ genapidoc.sh
@@ -7,8 +7,9 @@ rm -rf $output
 mkdir -p $output
 
 javadoc -d $output \
-`find toolkit-api -name '*.java'  -not -name '*XdsDocumentRegRep.java' -not -name '*XcaInitiatingGateway.java' -not -name '*XcaRespondingGateway.java' -not -name '*XdsDocumentConsumer.java'` \
+`find toolkit-api -name '*.java'  -not -name '*XdsDocumentRegRep.java' -not -name '*XcaInitiatingGateway.java' -not -name '*XcaRespondingGateway.java' -not -name '*XdsDocumentConsumer.java' -not -name '*EngineSpi.java' -not -name '*BasicSimParameters.java'` \
 `find toolkit-services-common -name '*.java' -not -name '*Resource.java' -not -name '*Adapter.java'` \
-`find toolkit-services-common -name DocumentResource`
+`find toolkit-services-common -name DocumentResource` \
+`find actorfactory -name SimulatorProperties.java`
 
 
--- toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/DocumentSource.java
+++ toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/DocumentSource.java
@@ -12,6 +12,9 @@ public interface DocumentSource extends AbstractActorInterface {
      * Send a raw Provide and Register transaction. Raw means that the user provides the SOAP Header,
      * SOAP Body and document map to send.  No updates are done the the metadata.  It is sent as provided.
      * The transactionType in the send request object is overwritten and need not be supplied.
+     * See <a href="https://bitbucket.org/bmajur/toolkit2/src/tip/it-tests/src/test/groovy/gov/nist/toolkit/itTests/xdr/XdrSrcSpec.groovy?at=develop&fileviewer=file-view-default">here</a>
+     * for test showing how this is used.
+     *
      * @param request raw request
      * @return raw response
      * @throws ToolkitServiceException if something goes wrong in the sending of the transaction. If the transaction
