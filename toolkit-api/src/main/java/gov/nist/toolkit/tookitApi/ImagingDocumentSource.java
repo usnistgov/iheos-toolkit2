63a649493e67e55337a3183e6facf965d6116951
bill <devnull@localhost>
2016-03-09 12:55:13 -0500

    Add api hooks.

--- toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/ImagingDocumentSource.java
+++ toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/ImagingDocumentSource.java
@@ -8,7 +8,7 @@ import gov.nist.toolkit.toolkitServicesCommon.DcmImageSet;
 /**
  * XDSI Image Document Source API
  */
-public interface ImageDocumentSource extends AbstractActorInterface {
+public interface ImagingDocumentSource extends AbstractActorInterface {
 
    DcmImageSet retrieveImagingDocumentSet(DcmImageSet request);
 }
--- toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/SimulatorBuilder.java
+++ toolkit-api/src/main/java/gov/nist/toolkit/tookitApi/SimulatorBuilder.java
@@ -62,6 +62,28 @@ public class SimulatorBuilder {
     }
 
     /**
+     * Create new Imaging Document Source simulator with default configuration.
+     * To create a simulator with
+     * custom configuration:
+     * <ol>
+     * <li>Create simulator with default configuration</li>
+     * <li>Update the local copy of the configuration</li>
+     * <li>Send the update via the update method</li>
+     * </ol>
+     * @param id Simulator ID
+     * @param user User creating Simulator.  Same as TestSession in Toolkit UI. The simulator ID must be unique for this user.
+     * @param environmentName Environment defines Affinity Domain coding schemes and TLS certificate for use with client.
+     * @return Simulator configuration.
+     * @throws ToolkitServiceException if anything goes wrong.
+     */
+    public ImagingDocumentSource createImagingDocumentSource(String id, String user, String environmentName) throws ToolkitServiceException {
+        XdsiImagingDocumentSource src = new XdsiImagingDocumentSource();
+        src.engine = engine;
+        src.config = engine.create(id, user, SimulatorActorType.DOCUMENT_SOURCE, environmentName);
+        return src;
+    }
+
+    /**
      * Create new Document Consumer simulator with default configuration.
      * To create a simulator with
      * custom configuration:
