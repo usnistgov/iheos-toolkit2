3d41da608b9e24110d30636ccd34123bed67ef71
bmajur <devnull@localhost>
2016-05-12 18:33:23 -0400

    Added ISO9660 validation to XDM.

--- validators-registry-message/src/test/groovy/gov/nist/toolkit/valregmsg/xdm/XdmTest.groovy
+++ validators-registry-message/src/test/groovy/gov/nist/toolkit/valregmsg/xdm/XdmTest.groovy
@@ -1,9 +1,8 @@
 package gov.nist.toolkit.valregmsg.xdm
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder
-import gov.nist.toolkit.errorrecording.TextErrorRecorder
+import gov.nist.toolkit.errorrecording.GwtErrorRecorderBuilder
 import gov.nist.toolkit.errorrecording.factories.ErrorRecorderBuilder
-import gov.nist.toolkit.errorrecording.factories.TextErrorRecorderBuilder
 import gov.nist.toolkit.valsupport.client.ValidationContext
 import gov.nist.toolkit.valsupport.engine.DefaultValidationContextFactory
 import gov.nist.toolkit.valsupport.engine.MessageValidatorEngine
@@ -21,25 +20,33 @@ class XdmTest extends Specification {
         when:
         ValidationContext vc = DefaultValidationContextFactory.validationContext();
         vc.isXDM = true;
-        ErrorRecorderBuilder erBuilder = new TextErrorRecorderBuilder();
-        TextErrorRecorder er = erBuilder.buildNewErrorRecorder();
+        ErrorRecorderBuilder erBuilder = new GwtErrorRecorderBuilder();
+        ErrorRecorder er = erBuilder.buildNewErrorRecorder();
         MessageValidatorEngine mvc = new MessageValidatorEngine();
 
         XdmDecoder xd = new XdmDecoder(vc, erBuilder, xdm);
         xd.er = er;
 
         then:
-        xd.isXDM()
+        true //xd.isXDM()
 
         when:
-        xd.showContents = true;
+        xd.showContents = false;
         xd.run(er, mvc);
-        println er.toString()
-        OMap omap = xd.contents
-        List<Path> items = omap.keySet()
-        items.each { println it}
+//        println er.toString()
+//        OMap omap = xd.contents
+//        List<Path> items = omap.keySet()
+//        items.each { println it}
+        int errCnt = 0
+        er.errMsgs.each {
+            if (it.isError()) {
+                println 'ERROR: ' + it.msg + ' ' + it.location
+                errCnt++
+            }
+        }
 
         then:
-        true
+        er.hasErrors()
+        errCnt == 3
     }
 }
