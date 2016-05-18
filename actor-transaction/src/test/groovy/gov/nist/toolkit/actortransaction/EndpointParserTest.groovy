568265d808ecf4fb1e5167a07ab2e35d66caf53a
bmajur <devnull@localhost>
2016-05-18 09:17:50 -0400

    Add EndpointParser

---
+++ actor-transaction/src/test/groovy/gov/nist/toolkit/actortransaction/EndpointParserTest.groovy
@@ -0,0 +1,55 @@
+package gov.nist.toolkit.actortransaction
+
+import spock.lang.Specification
+
+/**
+ *
+ */
+class EndpointParserTest extends Specification {
+
+    def 'null ok'() {
+        when: new EndpointParser(null)
+
+        then: notThrown Exception
+    }
+
+    def 'empty ok'() {
+        when: new EndpointParser('')
+
+        then: notThrown Exception
+    }
+
+    def 'space ok'() {
+        when: new EndpointParser(' ')
+
+        then: notThrown Exception
+    }
+
+    def 'good endpoint ok'() {
+        expect: new EndpointParser('http://host:port/foo').validate()
+    }
+
+    def 'http required'() {
+        expect: !new EndpointParser('ssh://host:port/foo').validate()
+    }
+
+    def 'no port ok'() {
+        expect: new EndpointParser('http://host/foo').validate()
+    }
+
+    def 'double slash is error'() {
+        expect: !new EndpointParser('http://host:port//foo').validate()
+    }
+
+    def 'parms is error'() {
+        expect: !new EndpointParser('http://host:port/foo?').validate()
+    }
+
+    def 'update hostname'() {
+        when: EndpointParser ep = new EndpointParser('http://host:port/foo')
+
+        then: ep.validate()
+
+        and: ep.updateHostAndPort('myhost', 'myport').getEndpoint() == 'http://myhost:myport/foo'
+    }
+}
