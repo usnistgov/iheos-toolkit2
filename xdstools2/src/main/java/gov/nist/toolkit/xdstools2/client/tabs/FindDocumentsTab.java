13a9480b3af6d51a35e8b1bcfdf5fc90c62e5cef
bmajur <devnull@localhost>
2016-06-09 09:20:56 -0400

    Create AbstractTool class.  Redo FindDocumentsTab to use it. (cherry picked from commit 64a4e34)

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/FindDocumentsTab.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/FindDocumentsTab.java
@@ -1,7 +1,5 @@
 package gov.nist.toolkit.xdstools2.client.tabs;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.CheckBox;
 import gov.nist.toolkit.configDatatypes.client.TransactionType;
 import gov.nist.toolkit.xdstools2.client.CoupledTransactions;
@@ -38,22 +36,13 @@ public class FindDocumentsTab extends AbstractTool {
 		mainGrid.setWidget(row, 0, selectOnDemand);
 		row++;
 
-		addQueryBoilerplate(new Runner(), transactionTypes, couplings, true);
+		requirePatientId();
+		declareTransactionTypes(transactionTypes);
 	}
 
-	class Runner implements ClickHandler {
-
-		public void onClick(ClickEvent event) {
-			resultPanel.clear();
-
-			if (!verifySiteProvided()) return;
-			if (!verifyPidProvided()) return;
-
-			prepareToRun();
-
-			toolkitService.findDocuments(queryBoilerplate.getSiteSelection(), pidTextBox.getValue().trim(), selectOnDemand.getValue(), queryCallback);
-		}
-
+	@Override
+	public void run() {
+		toolkitService.findDocuments(queryBoilerplate.getSiteSelection(), pidTextBox.getValue().trim(), selectOnDemand.getValue(), queryCallback);
 	}
 
 	public String getWindowShortName() {
--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/genericQueryTab/AbstractTool.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/genericQueryTab/AbstractTool.java
@@ -1,14 +1,25 @@
 package gov.nist.toolkit.xdstools2.client.tabs.genericQueryTab;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import gov.nist.toolkit.configDatatypes.client.TransactionType;
+import gov.nist.toolkit.xdstools2.client.CoupledTransactions;
+import gov.nist.toolkit.xdstools2.client.PopupMessage;
 import gov.nist.toolkit.xdstools2.client.TabContainer;
+
+import java.util.List;
 
 /**
  *
  */
 public abstract class AbstractTool extends GenericQueryTab {
+    private AbstractTool me = null;
+    private boolean checkSite = false;
+    private List<TransactionType> transactionTypes = null;
+    private CoupledTransactions couplings = new CoupledTransactions();
 
     /**
      * @return Short name to be displayed in tool tab
@@ -24,10 +35,19 @@ public abstract class AbstractTool extends GenericQueryTab {
         super(null);
     }
 
+    /**
+     * Tool specific initialization.
+     */
     abstract public void initTool();
+
+    /**
+     *
+     */
+    abstract public void run();
 
     @Override
     public void onTabLoad(TabContainer container, boolean select, String eventName) {
+        me = this;
         myContainer = container;
         topPanel = new VerticalPanel();
 
@@ -42,5 +62,56 @@ public abstract class AbstractTool extends GenericQueryTab {
         topPanel.add(mainGrid);
 
         initTool();
+
+        // TODO - throw error
+        if (transactionTypes == null && checkSite) {
+            new PopupMessage("Tool " + getToolTitle() + " does not declare any transactionTypes");
+            return;
+        }
+
+        addQueryBoilerplate(new Runner(), transactionTypes, couplings, hasPatientIdParam);
+    }
+
+    class Runner implements ClickHandler {
+
+        public void onClick(ClickEvent event) {
+            resultPanel.clear();
+
+            if (checkSite && !verifySiteProvided()) return;
+            if (hasPatientIdParam && !verifyPidProvided()) return;
+
+            prepareToRun();
+
+            run();
+        }
+
+    }
+
+    /**
+     * If true then declareTransactionTypes must be called declaring the transactions used.  This call
+     * must be made in initTool().  If true then run() will only be allowed to proceed if a site
+     * is selected. CheckSite is set to true by declareTransactionTypes - if you display
+     * selectable site then the validation that one is selected will be done when you hit the
+     * run button.
+     * @param checkSite
+     */
+    public void setCheckSite(boolean checkSite) {
+        this.checkSite = checkSite;
+    }
+
+    /**
+     * Display patient id input and validate it is filled in on run.
+     */
+    public void requirePatientId() {
+        hasPatientIdParam = true;
+    }
+
+    public void declareTransactionTypes(List<TransactionType> tt) {
+        transactionTypes = tt;
+        checkSite = true;
+    }
+
+    public void declareTransactionCouplings(CoupledTransactions ct) {
+        couplings = ct;
     }
 }
