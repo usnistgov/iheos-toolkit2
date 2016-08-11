dc4f7efc880475391fefd1b63c68d433a463c5b9
Bill Majurski <bmajur@gmail.com>
2016-08-11 14:55:28 -0400

    Remove suffix from PidBuilder - allowed hyphen followed by junk to be parsed out of PID.  But, PIDs can have hyphens in them.

--- config-datatypes/src/main/java/gov/nist/toolkit/configDatatypes/client/PidBuilder.java
+++ config-datatypes/src/main/java/gov/nist/toolkit/configDatatypes/client/PidBuilder.java
@@ -7,9 +7,13 @@ public class PidBuilder {
 
     // uses - char to separate pid from name
     // this is only important in UI displays
+
+    // bill - removed - split - some real PIDs have a - character in them
+
     public static Pid createPid(String patientId) {
-        String[] hyphenParts = patientId.split("-");
-        String[] parts = hyphenParts[0].split("\\^");
+//        String[] hyphenParts = patientId.split("-");
+//        String[] parts = hyphenParts[0].split("\\^");
+        String[] parts = patientId.split("\\^");
         if (parts.length < 4)
             return null;   // not valid pid
         String id = parts[0];
@@ -22,9 +26,9 @@ public class PidBuilder {
 
         Pid pid = new Pid(affinityDomainOid, id);
 
-        if (hyphenParts.length > 1) {
-            pid.setExtra(hyphenParts[1]);
-        }
+//        if (hyphenParts.length > 1) {
+//            pid.setExtra(hyphenParts[1]);
+//        }
 
         return pid;
     }
