7afd94b12f3df0231651045192bacdbef2a7c13f
Bill Majurski <bmajur@gmail.com>
2016-09-12 15:58:52 -0400

    Fix some error reporting issued on the Conformance tool.

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/conformanceTest/ConformanceTestTab.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/conformanceTest/ConformanceTestTab.java
@@ -183,7 +183,7 @@ public class ConformanceTestTab extends ToolWindow implements TestRunner, SiteMa
 		toolkitService.getSite(site, new AsyncCallback<Site>() {
 			@Override
 			public void onFailure(Throwable throwable) {
-//				new PopupMessage("getSiteName threw error: " + throwable.getMessage());
+				new PopupMessage("getSiteName threw error: " + throwable.getMessage());
 			}
 
 			@Override
