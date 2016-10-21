20fb0c579b3faa72240b0a010562a185f3c7068d
Diane Azais <diane.azais@gmail.com>
2016-10-21 14:00:10 -0400

    Integrate Assertions to validation error calls

--- validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/CxFormat.java
+++ validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/CxFormat.java
@@ -2,8 +2,10 @@ package gov.nist.toolkit.valregmetadata.datatype;
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
 import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 import gov.nist.toolkit.valregmetadata.field.ValidatorCommon;
+import org.junit.Assert;
 
 public class CxFormat extends FormatValidator {
 	private AssertionLibrary ASSERTIONLIBRARY = AssertionLibrary.getInstance();
@@ -15,9 +17,20 @@ public class CxFormat extends FormatValidator {
 	public void validate(String input) {
 
 		String error = ValidatorCommon.validate_CX_datatype(input);
+		String assertionID = "TA001";
 		if (error != null) {
-			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " : " + error, this, getResource(null));
-			//er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context, ASSERTIONLIBRARY.getErrorMessage(001), ASSERTIONLIBRARY.getLocation(001));
+			// Original call
+			//er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " : " + error, this, getResource(null));
+
+			// New call
+			Assertion a = ASSERTIONLIBRARY.getAssertion(assertionID);
+			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, a.getErrorMessage() + ": " + error + " - " + context + ": " + input, this,
+					 a.getLocation());
+			/*er.err(XdsErrorCode.Code.XDSRegistryMetadataError, a.getErrorMessage() + ": " + error + " - " + context + ": " + input, this,
+					a.getLocation());
+			String location = context + ": " + input;
+			String detail = error; //TODO rename the variable once changes are stable
+			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, assertion, this, location, detail);*/
 		}
 	}
 
