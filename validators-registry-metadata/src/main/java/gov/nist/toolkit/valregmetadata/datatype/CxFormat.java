71ac209d2f0799eaa8bafbdcdf7e07d4eb927a66
Diane Azais <diane.azais@gmail.com>
2016-10-26 13:45:48 -0400

    Cleanup

--- validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/CxFormat.java
+++ validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/CxFormat.java
@@ -19,18 +19,12 @@ public class CxFormat extends FormatValidator {
 		String error = ValidatorCommon.validate_CX_datatype(input);
 		String assertionID = "TA001";
 		if (error != null) {
+			// TODO remove comments after upgrade to Assertions model
 			// Original call
 			//er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " : " + error, this, getResource(null));
 
 			// New call
 			Assertion assertion = ASSERTIONLIBRARY.getAssertion(assertionID);
-			/*er.err(XdsErrorCode.Code.XDSRegistryMetadataError, a.getErrorMessage() + ": " + error + " - " + context + ": " + input, this,
-					 a.getLocation());
-					 */
-			/*
-			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, a.getErrorMessage() + ": " + error + " - " + context + ": " + input, this,
-					a.getLocation());
-					*/
 			String location = context + ": " + input;
 			String detail = error; //TODO rename the variable once changes are stable
 			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, assertion, this, location, detail);
