5ac70fcc78efff64301472326de9d936c99651fb
Diane Azais <diane.azais@gmail.com>
2016-10-27 14:42:29 -0400

    Add TA018 to DTMFormat validator

--- validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/DtmFormat.java
+++ validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/DtmFormat.java
@@ -2,20 +2,30 @@ package gov.nist.toolkit.valregmetadata.datatype;
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
+import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 import gov.nist.toolkit.valregmetadata.field.ValidatorCommon;
 
 public class DtmFormat extends FormatValidator {
+	private AssertionLibrary ASSERTIONLIBRARY = AssertionLibrary.getInstance();
+
 
 	public DtmFormat(ErrorRecorder er, String context, String resource) {
 		super(er, context, resource);
 	}
 
 	public void validate(String input) {
+		Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA018");
+
 		int size = input.length();
-		if (!(size == 4 || size == 6 || size == 8 || size == 10 || size == 12 || size == 14))
-			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " is not in HL7 V2 DateTime format: it has an invalid number of characters", this, getResource("ITI TF-3: Table 4.1-3"));
-		if (!ValidatorCommon.isInt(input) )
-			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " is not in HL7 V2 DateTime format: all characters must be digits", this, getResource("ITI TF-3: Table 4.1-3"));
+		if (!(size == 4 || size == 6 || size == 8 || size == 10 || size == 12 || size == 14)) {
+			String detail = "Input " + input + " has an invalid number of characters.";
+			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, assertion, this, context, detail);
+		}
+		if (!ValidatorCommon.isInt(input)) {
+			String detail = "Input: " + input + ". All characters must be digits.";
+			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, assertion, this, context, detail);
+		}
 	}
 
 }
