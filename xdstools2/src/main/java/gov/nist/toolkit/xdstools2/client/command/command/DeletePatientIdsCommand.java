ddd889819678559170dd6528fb1586356f8081c6
Herrmann, Olivier (IntlAssoc) <olivier.herrmann@telecomnancy.net>
2016-11-07 15:00:49 -0500

    addPatientIds, deletePatientIds, getCollection, getCollectionNames, getCollectionMembers, getPatientIds, getTestCollections, getTestIndex, getTestReadme, runMesaTest, runTest rpc calls were refactored to use the command pattern

---
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/command/command/DeletePatientIdsCommand.java
@@ -0,0 +1,7 @@
+package gov.nist.toolkit.xdstools2.client.command.command;
+
+/**
+ * Created by onh2 on 11/7/16.
+ */
+public class DeletePatientIdsCommand {
+}
---
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/shared/command/request/GetCollectionRequest.java
@@ -0,0 +1,7 @@
+package gov.nist.toolkit.xdstools2.shared.command.request;
+
+/**
+ * Created by onh2 on 11/7/16.
+ */
+public class GetCollectionRequest {
+}
