8b051e17a836e9ff4ea401c1e63fe5efe3728a56
Diane Azais <diane.azais@gmail.com>
2016-11-09 15:26:25 -0500

    Notes for future work on IntFormat assertions

--- validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/IntFormat.java
+++ validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/IntFormat.java
@@ -2,6 +2,8 @@ package gov.nist.toolkit.valregmetadata.datatype;
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
+import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 import gov.nist.toolkit.valregmetadata.field.ValidatorCommon;
 
 public class IntFormat extends FormatValidator {
@@ -10,6 +12,7 @@ public class IntFormat extends FormatValidator {
 		super(er, context, resource);
 	}
 
+	//TODO add Assertions model after figuring out value of Resource
 	public void validate(String input) {
 		if (!ValidatorCommon.isInt(input)) {
 			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " is not an integer", this, getResource(null));
