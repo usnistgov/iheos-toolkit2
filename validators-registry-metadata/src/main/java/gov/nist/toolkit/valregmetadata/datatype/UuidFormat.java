03d372516894950fcd3881ed17936d1d19c04e6b
Diane Azais <diane.azais@gmail.com>
2016-11-10 00:35:13 -0500

    Add TA091 to UuidFormat

--- validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/UuidFormat.java
+++ validators-registry-metadata/src/main/java/gov/nist/toolkit/valregmetadata/datatype/UuidFormat.java
@@ -2,8 +2,11 @@ package gov.nist.toolkit.valregmetadata.datatype;
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
+import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 
 public class UuidFormat extends FormatValidator {
+	private AssertionLibrary ASSERTIONLIBRARY = AssertionLibrary.getInstance();
 
 	public UuidFormat(ErrorRecorder er, String context, String resource) {
 		super(er, context, resource);
@@ -11,7 +14,9 @@ public class UuidFormat extends FormatValidator {
 
 	public void validate(String input) {
 		if (!isUuid(input)) {
-			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, context + ": " + input + " is not in UUID format", this, getResource("ITI TF-3: Table 4.1-3 and section 4.1.12.3"));
+			Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA091");
+			String location = context + ": " + input;
+			er.err(XdsErrorCode.Code.XDSRegistryMetadataError, assertion, this, location, "");
 		}
 	}
 
