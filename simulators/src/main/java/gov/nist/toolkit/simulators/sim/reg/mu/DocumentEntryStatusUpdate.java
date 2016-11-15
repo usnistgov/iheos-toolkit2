160e38491908a35780a0aaa91adc3d44691a727d
Diane Azais <diane.azais@gmail.com>
2016-11-15 11:11:26 -0500

    Add TA101, TA102 to DocumentEntryStatusUpdate

--- simulators/src/main/java/gov/nist/toolkit/simulators/sim/reg/mu/DocumentEntryStatusUpdate.java
+++ simulators/src/main/java/gov/nist/toolkit/simulators/sim/reg/mu/DocumentEntryStatusUpdate.java
@@ -2,7 +2,10 @@ package gov.nist.toolkit.simulators.sim.reg.mu;
 
 import gov.nist.toolkit.actorfactory.client.SimulatorConfig;
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
+import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode.Code;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
+import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 import gov.nist.toolkit.registrymetadata.Metadata;
 import gov.nist.toolkit.simulators.sim.reg.RegRSim;
 import gov.nist.toolkit.simulators.sim.reg.store.DocEntry;
@@ -17,7 +20,8 @@ import org.apache.axiom.om.OMElement;
 public class DocumentEntryStatusUpdate extends RegRSim {
 	ErrorRecorder er;
 	DsSimCommon dsSimCommon;
-	
+	private AssertionLibrary ASSERTIONLIBRARY = AssertionLibrary.getInstance();
+
 	public DocumentEntryStatusUpdate(SimCommon common, DsSimCommon dsSimCommon, ErrorRecorder er, SimulatorConfig asc) {
 		super(common, dsSimCommon, asc);
 		this. dsSimCommon = dsSimCommon;
@@ -25,42 +29,40 @@ public class DocumentEntryStatusUpdate extends RegRSim {
 	}
 
 	public void run(MuSim muSim, Metadata m, OMElement assoc, DocEntry docEntry, String origStatus, String newStatus) {
-		
+
 		try {
 			er.detail("Update trigger is Association(" + m.getId(assoc) + ") " + m.getSimpleAssocType(assoc) + "\n" +
 					"\t\tsourceObject=" + m.getAssocSource(assoc)  +
-					"\t\ttargetObject=" + m.getAssocTarget(assoc)  
+					"\t\ttargetObject=" + m.getAssocTarget(assoc)
 			);
 		} catch (Exception e) {}
-		
+
 		String prefix = "Update (trigger=Association(" + muSim.UUIDToSymbolic.get(m.getId(assoc)) +")) - ";
 
-		
+
 		vc.addMetadataPattern(MetadataPattern.UpdateDocumentEntryStatus);
-		
+
 		StatusValue origStatValue = RegIndex.getStatusValue(origStatus);
 		if (origStatValue != docEntry.getAvailabilityStatus()) {
-			er.err(Code.XDSMetadataUpdateError, 
-					prefix + "OriginalStatus Slot on UpdateAvailabilityStatus Association (" + origStatValue + " does not match existing availabilityStatus on DocumentEntry(" +
-					docEntry.getId() + ") " + docEntry.getAvailabilityStatus(),
-					this, "ITI TF-2b:3.57.4.1.3.3.2.3");
-
+			Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA101");
+			String detail = "Found: '" + prefix + "'; " + "originalStatus value found: '" + origStatValue + "'; " +
+					"availabilityStatus on DocumentEntry: '" + docEntry.getId() + " " + docEntry.getAvailabilityStatus() + "'";
+			er.err(XdsErrorCode.Code.XDSMetadataUpdateError, assertion, this, "", detail);
 		}
-		
+
 		String lid = docEntry.lid;
-		
+
 		DocEntry latest = muSim.delta.docEntryCollection.getLatestVersion(lid);
-		
+
 		String latestId = latest.getId();
 		String targetId = m.getAssocTarget(assoc);
-		
+
 		if (!latestId.equals(targetId)) {
-			er.err(Code.XDSMetadataUpdateError, 
-					prefix + "target DocumentEntry(" + latestId +") is not the most recent version",
-					this, "ITI TF-2b:3.57.4.1.3.3.2.3");
+			Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA102");
+			String detail = "Found: '" + prefix + "'; " + "target DocumentEntry ID found: '" + latestId +"'";
+			er.err(XdsErrorCode.Code.XDSMetadataUpdateError, assertion, this, "", detail);
 		}
 
-		
 		if (!er.hasErrors()) {
 			muSim.save(m, true);
 			// perform status change
