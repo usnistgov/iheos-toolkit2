ab5193efeab3bc3a4f72e6795d6a149713a39b5b
Diane Azais <diane.azais@gmail.com>
2016-11-25 15:03:55 -0500

    Add TA151, TA152 to RetrieveResponseValidator

--- validators-registry-message/src/main/java/gov/nist/toolkit/valregmsg/message/RetrieveResponseValidator.java
+++ validators-registry-message/src/main/java/gov/nist/toolkit/valregmsg/message/RetrieveResponseValidator.java
@@ -2,6 +2,8 @@ package gov.nist.toolkit.valregmsg.message;
 
 import gov.nist.toolkit.errorrecording.ErrorRecorder;
 import gov.nist.toolkit.errorrecording.client.XdsErrorCode;
+import gov.nist.toolkit.errorrecording.client.assertions.Assertion;
+import gov.nist.toolkit.errorrecording.client.assertions.AssertionLibrary;
 import gov.nist.toolkit.errorrecording.factories.ErrorRecorderBuilder;
 import gov.nist.toolkit.utilities.xml.XmlUtil;
 import gov.nist.toolkit.valsupport.client.ValidationContext;
@@ -20,6 +22,8 @@ public class RetrieveResponseValidator extends AbstractMessageValidator {
 	OMElement xml;
 	ErrorRecorderBuilder erBuilder;
 	MessageValidatorEngine mvc;
+	private AssertionLibrary ASSERTIONLIBRARY = AssertionLibrary.getInstance();
+
 
 	public RetrieveResponseValidator(ValidationContext vc, OMElement xml, ErrorRecorderBuilder erBuilder, MessageValidatorEngine mvc) {
 		super(vc);
@@ -33,14 +37,17 @@ public class RetrieveResponseValidator extends AbstractMessageValidator {
 		er.registerValidator(this);
 
 		if (xml == null) {
-			er.err(XdsErrorCode.Code.XDSRegistryError, "RetrieveDocumentSetResponse: top element null", this, "");
+			Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA151");
+			er.err(XdsErrorCode.Code.XDSRegistryError, assertion, this, "", "");
             er.unRegisterValidator(this);
 			return;
 		}
 
 		OMElement registryResponse = XmlUtil.firstChildWithLocalName(xml, "RegistryResponse");
-		if (registryResponse == null)
-			er.err(XdsErrorCode.Code.XDSRegistryError, "RegistryResponse missing", this, "Schema");
+		if (registryResponse == null) {
+			Assertion assertion = ASSERTIONLIBRARY.getAssertion("TA152");
+			er.err(XdsErrorCode.Code.XDSRegistryError, assertion, this, "", "");
+		}
 		else {
 			mvc.addMessageValidator("RegistryResponse", new RegistryResponseValidator(vc, registryResponse), erBuilder.buildNewErrorRecorder());
 		}
