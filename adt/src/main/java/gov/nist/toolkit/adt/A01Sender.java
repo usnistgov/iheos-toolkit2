a2ccd38e9be2b1ae4db72f1086bfece97c9e03e4
Bill Majurski <bmajur@gmail.com>
2016-12-15 14:07:17 -0500

    Fixed #153 - EVN segment missing from PIX Feed (v2) message.

--- adt/src/main/java/gov/nist/toolkit/adt/A01Sender.java
+++ adt/src/main/java/gov/nist/toolkit/adt/A01Sender.java
@@ -9,11 +9,12 @@ import java.io.IOException;
  */
 public class A01Sender {
     static Logger logger = Logger.getLogger(A01Sender.class);
-    private static String[] A01 = new String[3];
+    private static String[] A01 = new String[4];
     static {
         A01[0] = "MSH|^~\\&|EPICADT|DH|LABADT|DH|201301011226||ADT^A01|HL7MSG00001|P|2.3|";
-        A01[1] = "PID|||$pid$||APPLESEED^JOHN^A^III||19710101|M||C|1^CATALYZE STREET^^MADISON^WI^53005-1020|GL|(414)379-1212|(414)271-3434||S||MRN12345001^2^M10|123456789|987654^NC|";
-        A01[2] = "PV1||I";
+        A01[1] = "EVN||20090224104145-0600";
+        A01[2] = "PID|||$pid$||APPLESEED^JOHN^A^III||19710101|M||C|1^CATALYZE STREET^^MADISON^WI^53005-1020|GL|(414)379-1212|(414)271-3434||S||MRN12345001^2^M10|123456789|987654^NC|";
+        A01[3] = "PV1||I";
     }
 
     static public void send(String server, int port, String patientId) throws IOException {
