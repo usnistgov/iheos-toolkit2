019b3f33d95ad45af5d96f72cfaa132fa66ddd59
Herrmann, Olivier (IntlAssoc) <olivier.herrmann@telecomnancy.net>
2017-01-09 13:39:49 -0500

    PidEditTab simple code refactor

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/simulatorControlTab/PidEditTab.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/simulatorControlTab/PidEditTab.java
@@ -26,6 +26,7 @@ public class PidEditTab extends GenericQueryTab {
     SimId simId;
     ListBox pidList = new ListBox();
     TextArea pidBox = new TextArea();
+    Anchor reload = null;
 
     public PidEditTab(SimulatorConfig config) {
         super(new FindDocumentsSiteActorManager());
@@ -34,60 +35,36 @@ public class PidEditTab extends GenericQueryTab {
     }
 
     @Override
+    public String getWindowShortName() {
+        return "PidEditTab";
+    }
+
+    @Override
     protected Widget buildUI() {
-        return null;
-    }
+        FlowPanel flowPanel=new FlowPanel();
+        flowPanel.add(new HTML("<h2>Registry Simulator Patient ID Display/Edit</h2>"));
+        flowPanel.add(new HTML("<h3>Simulator " + simId.toString() + "</h3>"));
 
-    @Override
-    protected void bindUI() {
-
-    }
-
-    @Override
-    protected void configureTabView() {
-
-    }
-
-    @Override
-    public void onTabLoad(boolean select, String eventName) {
-        registerTab(select, "PidEdit");
+        mainGrid=new FlexTable();
+        flowPanel.add(mainGrid);
 
         addReloader();
 
-        tabTopPanel.add(new HTML("<h2>Registry Simulator Patient ID Display/Edit</h2>"));
-        tabTopPanel.add(new HTML("<h3>Simulator " + simId.toString() + "</h3>"));
-
         HorizontalPanel panel = new HorizontalPanel();
-        tabTopPanel.add(panel);
+        flowPanel.add(panel);
+
         VerticalPanel listPanel = new VerticalPanel();
         panel.add(listPanel);
         listPanel.add(new HTML("Registered Patient IDs"));
+
         pidList.setVisibleItemCount(25);
         pidList.setMultipleSelect(true);
         listPanel.add(pidList);
+
         Button deleteButton = new Button("Delete", new ClickHandler() {
             @Override
             public void onClick(ClickEvent clickEvent) {
-                List<String> toDelete = new ArrayList<>();
-                for (int i=0; i<pidList.getItemCount(); i++) {
-                    if (pidList.isItemSelected(i)) {
-                        toDelete.add(pidList.getItemText(i));
-                    }
-                }
-                if (toDelete.size() == 0) new PopupMessage("Nothing selected to delete");
-                else {
-                    List<Pid> pidsToDelete = new ArrayList<>();
-                    for (String pidString : toDelete) {
-                        Pid p = PidBuilder.createPid(pidString);
-                        if (p != null) pidsToDelete.add(p);
-                    }
-                    new DeletePatientIdsCommand(){
-                        @Override
-                        public void onComplete(Boolean result) {
-                            loadPids();
-                        }
-                    }.run(new PatientIdsRequest(getCommandContext(),simId,pidsToDelete));
-                }
+                deletePid();
             }
         });
         listPanel.add(deleteButton);
@@ -103,55 +80,92 @@ public class PidEditTab extends GenericQueryTab {
         pidPanel.add(new Button("Add", new ClickHandler() {
             @Override
             public void onClick(ClickEvent clickEvent) {
-                final String value = pidBox.getValue();
-                List<String> stringValues = formatIds(value);
-                final List<Pid> pids = new ArrayList<>();
-                List<String> badPidList = new ArrayList<>();
-                for (String stringValue : stringValues) {
-                    Pid pid = PidBuilder.createPid(stringValue);
-                    if (pid == null) {
-                        badPidList.add(stringValue);
-                    } else {
-                        pids.add(pid);
-                    }
-                }
-                if (badPidList.size() > 0)
-                    new PopupMessage("These are not properly formatted Patient IDs, they will be ignored - " + badPidList);
-                if (pids.size() == 0) {
-                    new PopupMessage("Enter some Patient IDs");
-                    clear();
-                    return;
-                }
-                new AddPatientIdsCommand(){
-                    @Override
-                    public void onComplete(String o) {
-                        int dups = 0;
-                        int added = 0;
-                        for (Pid pid : pids) {
-                            String s = pid.asString();
-                            if (contains(s)) { dups++; continue; }
-                            added++;
-                            pidList.insertItem(s, 0);
-                        }
-                        clear();
-                        if (dups == 0) new PopupMessage(added + " Patient IDs added");
-                        else if (dups == 1) new PopupMessage("1 Patient ID added, others were duplicates");
-                        else new PopupMessage(added + " Patient IDs added, " + dups + "  were duplicates");
-                    }
-                }.run(new PatientIdsRequest(getCommandContext(),simId,pids));
+                addPid();
             }
-
-            boolean contains(String pidString) {
-                for (int i=0; i<pidList.getItemCount(); i++) {
-                    if (pidString.equals(pidList.getItemText(i))) return true;
-                }
-                return false;
-            }
-
-            void clear() { pidBox.setText(""); }
         }));
 
+        return flowPanel;
+    }
+
+    @Override
+    protected void bindUI() {
         loadPids();
+    }
+
+    @Override
+    protected void configureTabView() {
+
+    }
+
+    boolean containsPid(String pidString) {
+        for (int i=0; i<pidList.getItemCount(); i++) {
+            if (pidString.equals(pidList.getItemText(i))) return true;
+        }
+        return false;
+    }
+
+    private void addPid() {
+        final String value = pidBox.getValue();
+        List<String> stringValues = formatIds(value);
+        final List<Pid> pids = new ArrayList<>();
+        List<String> badPidList = new ArrayList<>();
+        for (String stringValue : stringValues) {
+            Pid pid = PidBuilder.createPid(stringValue);
+            if (pid == null) {
+                badPidList.add(stringValue);
+            } else {
+                pids.add(pid);
+            }
+        }
+        if (badPidList.size() > 0)
+            new PopupMessage("These are not properly formatted Patient IDs, they will be ignored - " + badPidList);
+        if (pids.size() == 0) {
+            new PopupMessage("Enter some Patient IDs");
+            // clear pid input field
+            pidBox.setText("");
+            return;
+        }
+        new AddPatientIdsCommand(){
+            @Override
+            public void onComplete(String o) {
+                int dups = 0;
+                int added = 0;
+                for (Pid pid : pids) {
+                    String s = pid.asString();
+                    if (containsPid(s)) { dups++; continue; }
+                    added++;
+                    pidList.insertItem(s, 0);
+                }
+                // clear pid input field
+                pidBox.setText("");
+                if (dups == 0) new PopupMessage(added + " Patient IDs added");
+                else if (dups == 1) new PopupMessage("1 Patient ID added, others were duplicates");
+                else new PopupMessage(added + " Patient IDs added, " + dups + "  were duplicates");
+            }
+        }.run(new PatientIdsRequest(getCommandContext(),simId,pids));
+    }
+
+    private void deletePid() {
+        List<String> toDelete = new ArrayList<>();
+        for (int i=0; i<pidList.getItemCount(); i++) {
+            if (pidList.isItemSelected(i)) {
+                toDelete.add(pidList.getItemText(i));
+            }
+        }
+        if (toDelete.size() == 0) new PopupMessage("Nothing selected to delete");
+        else {
+            List<Pid> pidsToDelete = new ArrayList<>();
+            for (String pidString : toDelete) {
+                Pid p = PidBuilder.createPid(pidString);
+                if (p != null) pidsToDelete.add(p);
+            }
+            new DeletePatientIdsCommand(){
+                @Override
+                public void onComplete(Boolean result) {
+                    loadPids();
+                }
+            }.run(new PatientIdsRequest(getCommandContext(),simId,pidsToDelete));
+        }
     }
 
     private void loadPids() {
@@ -163,12 +177,6 @@ public class PidEditTab extends GenericQueryTab {
             }
         }.run(new PatientIdsRequest(getCommandContext(),simId));
     }
-
-    public String getWindowShortName() {
-        return "PidEditTab";
-    }
-
-    Anchor reload = null;
 
     public void addReloader() {
         if (reload == null) {
