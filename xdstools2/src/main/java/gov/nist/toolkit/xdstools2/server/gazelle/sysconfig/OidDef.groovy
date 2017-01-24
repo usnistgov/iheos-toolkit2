e7a2408213befc3fdae55dfc296b4b0eec6214e3
Bill Majurski <bmajur@gmail.com>
2017-01-23 22:52:24 -0500

    Repairs to gazelle system configuration pull
    (cherry picked from commit 8d31d81)

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/server/gazelle/sysconfig/OidDef.groovy
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/server/gazelle/sysconfig/OidDef.groovy
@@ -14,7 +14,7 @@ class OidDef {
     // Type values
     static final String OrganizationOid = 'organization OID'
     static final String RepUidOid = 'repositoryUniqueID OID'
-    static final String ODDSRepUidOid = 'repositoryUniqueID-OnDemandDocSrc'
+    static final String ODDSRepUidOid = 'repositoryUniqueID OID for On-Demand Doc Src'
     static final String IntSrcRepoUidOid = 'repositoryUniqueID-IntegSrcRepos'
     static final String HomeIdOid = 'homeCommunityID OID'
     static final String AssigningAuthorityOid = 'patient ID assigning authority OID'
