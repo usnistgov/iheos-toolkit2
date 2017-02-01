6a3eadc7aa8524c44e25e4777a3c863ce1b25bf4
Bill Majurski <bmajur@gmail.com>
2017-02-01 14:50:13 -0500

    A start

---
+++ testclient/src/main/java/gov/nist/toolkit/testclient/client/util/ClientFactory.java
@@ -0,0 +1,24 @@
+package gov.nist.toolkit.testclient.client.util;
+
+import com.google.gwt.place.shared.PlaceController;
+import com.google.web.bindery.event.shared.EventBus;
+import gov.nist.toolkit.xdstools2.client.util.activitiesAndPlaces.ConfActorActivity;
+import gov.nist.toolkit.xdstools2.client.util.activitiesAndPlaces.SimLogActivity;
+import gov.nist.toolkit.xdstools2.client.util.activitiesAndPlaces.TestInstanceActivity;
+import gov.nist.toolkit.xdstools2.client.util.activitiesAndPlaces.ToolActivity;
+
+/**
+ * This is a factory that provides references to objects needed
+ * throughout the application like the event bus.
+ */
+public interface ClientFactory {
+    EventBus getEventBus();
+    PlaceController getPlaceController();
+    TestInstanceActivity getTestInstanceActivity();
+    ToolActivity getToolActivity();
+    ConfActorActivity getConfActorActivity();
+
+    TestClientServiceAsync getToolkitServices();
+
+    SimLogActivity getSimLogActivity();
+}
