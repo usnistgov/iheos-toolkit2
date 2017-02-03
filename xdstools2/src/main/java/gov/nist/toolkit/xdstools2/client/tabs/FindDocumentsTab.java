f5a473a75b5c35eddc2c813d1c821107fce21b7e
Sunil Bhaskarla <skbhaskarla@gmail.com>
2017-02-03 15:31:16 -0500

    Cleanup dead code.

--- xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/FindDocumentsTab.java
+++ xdstools2/src/main/java/gov/nist/toolkit/xdstools2/client/tabs/FindDocumentsTab.java
@@ -1,11 +1,9 @@
 package gov.nist.toolkit.xdstools2.client.tabs;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.CheckBox;
 import gov.nist.toolkit.configDatatypes.client.Pid;
 import gov.nist.toolkit.configDatatypes.client.TransactionType;
 import gov.nist.toolkit.interactionmodel.client.InteractingEntity;
-import gov.nist.toolkit.interactionmodel.client.InteractionIdentifierTerm;
 import gov.nist.toolkit.results.client.Result;
 import gov.nist.toolkit.sitemanagement.client.SiteSpec;
 import gov.nist.toolkit.xdstools2.client.CoupledTransactions;
@@ -92,113 +90,4 @@ public class FindDocumentsTab extends AbstractTool {
             }
         }.run(new FindDocumentsRequest(getCommandContext(),queryBoilerplate.getSiteSelection(), pidTextBox.getValue().trim(), selectOnDemand.getValue()));
     }
-
-
-    public InteractingEntity getInteractionModel() {
-        // begin interaction model
-        InteractingEntity registryEntity = new InteractingEntity(); // Destination
-
-        origin.setName(null); // Matches with the transactionSettings origin. null=TestClient
-        origin.setDescription("Document Consumer - Toolkit");
-
-        registryEntity.setName(getSiteSelection().getName());
-        registryEntity.setDescription("Registry - SUT");
-        registryEntity.setSourceInteractionLabel("Stored Query (ITI-18)");
-
-        List<InteractionIdentifierTerm> identifierTerms = new ArrayList<>();
-        InteractionIdentifierTerm identifierTerm
-                = new InteractionIdentifierTerm("$patient_id$", InteractionIdentifierTerm.Operator.EQUALTO, pidTextBox.getValue().trim());
-        identifierTerms.add(identifierTerm);
-        registryEntity.setInteractionIdentifierTerms(identifierTerms);
-
-        origin.setInteractions(new ArrayList<InteractingEntity>());
-        origin.getInteractions().add(registryEntity);
-
-        // end
-        return origin;
-    }
-
-
-    public InteractingEntity testIG() {
-        InteractingEntity initiator = new InteractingEntity();
-        initiator.setName("Toolkit");
-
-        InteractingEntity ig = new InteractingEntity();
-        ig.setName("IG");
-
-        InteractingEntity rg1 = new InteractingEntity("rg1");
-        InteractingEntity reg1 = new InteractingEntity("reg1");
-        InteractingEntity rep1 = new InteractingEntity("rep1");
-
-        rg1.setInteractions(new ArrayList<InteractingEntity>());
-        rg1.getInteractions().add(reg1);
-        rg1.getInteractions().add(rep1);
-
-        ig.setInteractions(new ArrayList<InteractingEntity>());
-        ig.getInteractions().add(rg1);
-
-        InteractingEntity rg2 = new InteractingEntity("rg2");
-        InteractingEntity reg2 = new InteractingEntity("reg2");
-        InteractingEntity rep2 = new InteractingEntity("rep2");
-
-        rg2.setInteractions(new ArrayList<InteractingEntity>());
-        rg2.getInteractions().add(reg2);
-        rg2.getInteractions().add(rep2);
-
-        ig.getInteractions().add(rg2);
-
-        initiator.setInteractions(new ArrayList<InteractingEntity>());
-        initiator.getInteractions().add(ig);
-
-        return initiator;
-    }
-
-    public InteractingEntity testReuseLL() {
-        InteractingEntity initiator = new InteractingEntity();
-        initiator.setName("Toolkit");
-
-        InteractingEntity ig = new InteractingEntity();
-        ig.setName("IG");
-
-        InteractingEntity rg1 = new InteractingEntity("rg1");
-        InteractingEntity reg1 = new InteractingEntity("reg1");
-        InteractingEntity rep1 = new InteractingEntity("rep1");
-
-        rg1.setInteractions(new ArrayList<InteractingEntity>());
-        rg1.getInteractions().add(reg1);
-        rg1.getInteractions().add(rep1);
-
-        ig.setInteractions(new ArrayList<InteractingEntity>());
-        ig.getInteractions().add(rg1);
-
-        ig.getInteractions().add(rep1);
-
-        return ig;
-    }
-
-    public List<InteractingEntity> testTwoActors() {
-
-        List<InteractingEntity> interactingEntityList = new ArrayList<InteractingEntity>();
-
-        InteractingEntity initiator = new InteractingEntity();
-        initiator.setName("Tc/Rep");
-
-
-        InteractingEntity reg = new InteractingEntity("reg");
-
-        initiator.setInteractions(new ArrayList<InteractingEntity>());
-        initiator.getInteractions().add(reg);
-
-        InteractingEntity initiator2 = new InteractingEntity();
-        initiator2.setName("Tc/DocCons");
-        initiator2.setInteractions(new ArrayList<InteractingEntity>());
-        initiator2.getInteractions().add(reg);
-
-        interactingEntityList.add(initiator);
-        interactingEntityList.add(initiator2);
-
-        return interactingEntityList;
-
-    }
-
 }
