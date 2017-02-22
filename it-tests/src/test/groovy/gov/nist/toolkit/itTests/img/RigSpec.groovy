6a63f862aaf99ee82f0d89c461b90692a450b14b
davidmaffitt <david.maffitt@wustl.edu>
2017-02-22 12:48:00 -0600

    progress on RigSpec

---
+++ it-tests/src/test/groovy/gov/nist/toolkit/itTests/img/RigSpec.groovy
@@ -0,0 +1,109 @@
+package gov.nist.toolkit.itTests.img
+
+import gov.nist.toolkit.actorfactory.SimManager
+import gov.nist.toolkit.actorfactory.client.SimId
+import gov.nist.toolkit.adt.ListenerFactory
+import gov.nist.toolkit.itTests.support.ToolkitSpecification
+import gov.nist.toolkit.results.client.TestInstance
+import gov.nist.toolkit.services.client.IdsOrchestrationRequest
+import gov.nist.toolkit.services.client.IdsOrchestrationResponse
+import gov.nist.toolkit.services.client.RawResponse
+import gov.nist.toolkit.services.client.RigOrchestrationRequest
+import gov.nist.toolkit.services.client.RigOrchestrationResponse
+import gov.nist.toolkit.services.server.orchestration.IdsOrchestrationBuilder
+import gov.nist.toolkit.services.server.orchestration.RigOrchestrationBuilder
+import gov.nist.toolkit.session.client.logtypes.TestOverviewDTO
+import gov.nist.toolkit.sitemanagement.Sites
+import gov.nist.toolkit.sitemanagement.client.Site
+import gov.nist.toolkit.sitemanagement.client.SiteSpec
+import gov.nist.toolkit.testengine.scripts.BuildCollections
+import gov.nist.toolkit.toolkitApi.SimulatorBuilder
+import gov.nist.toolkit.toolkitServicesCommon.SimConfig
+import spock.lang.Shared
+/**
+ * Integration tests for RIG Simulator
+ */
+class RigSpec extends ToolkitSpecification {
+    @Shared SimulatorBuilder spi
+    @Shared String testSession = 'rigspec';
+    @Shared String id = 'simulator_rig'
+    @Shared SimId simId = new SimId(testSession, id)
+    @Shared String envName = 'default'
+    @Shared SimConfig sutSimConfig
+
+
+    @Shared String urlRoot = String.format("http://localhost:%s/xdstools2", remoteToolkitPort)
+
+    /**
+     * Run once at class initialization.
+     * @return
+     */
+    def setupSpec() {
+        // Opens a grizzly server for testing, in lieu of tomcat
+        startGrizzly('8889')
+
+        // Initialize remote api for talking to toolkit on Grizzly
+        // Needed to build simulators
+        spi = getSimulatorApi(remoteToolkitPort)
+
+        // builds his test collections. mimics startup of tomcat
+        new BuildCollections().init(null)
+
+        // creates the special user/session for this test
+        api.createTestSession(testSession)
+    }
+
+    // one time shutdown when everything is done
+    def cleanupSpec() {
+        server.stop()
+        ListenerFactory.terminateAll()
+    }
+
+    def setup() {
+    }
+
+    def 'rig tests' () {
+        setup: 'rig orchestration request is set up'
+        RigOrchestrationRequest request = new RigOrchestrationRequest()
+        request.environmentName = envName
+        request.userName = testSession
+        request.useExistingSimulator = false
+        request.siteUnderTest = new SiteSpec(simId.toString())
+
+        when: 'build orchestration'
+        def builder = new RigOrchestrationBuilder(api, session, request)
+        RawResponse rawResponse = builder.buildTestEnvironment()
+
+        then: 'return is instance of RigOrchestrationResponse'
+        rawResponse != null
+        rawResponse instanceof RigOrchestrationResponse
+
+        when: 'cast response to RigOrchestrationResponse'
+        RigOrchestrationResponse response = (RigOrchestrationResponse) rawResponse
+
+        then: 'orchestration completed successfully'
+        !response.isError()
+
+        when: 'run test'
+        SiteSpec siteSpec = request.siteUnderTest
+
+        SimManager simManager = new SimManager(api.getSession().id)
+        Sites sites = simManager.getAllSites(new Sites())
+        Site sutSite = sites.getSite(siteSpec.name)
+
+        TestInstance testInstance = new TestInstance(testId)
+        List<String> sections = []
+        Map<String, String> params = new HashMap<>()
+
+        TestOverviewDTO testOverviewDTO = session.xdsTestServiceManager().runTest(envName, testSession, siteSpec, testInstance, sections, params, null, true)
+
+        then: 'Returned test status is pass'
+        testOverviewDTO.pass
+
+        where: "tests to run include:"
+        testId         || valid
+        "rig_5420"     || true
+        "rig_5421"     || true
+    }
+
+}
