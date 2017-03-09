45661ee5b6d28b6d2ebde6171af9df209747a987
Diane Azais <diane.azais@gmail.com>
2017-03-09 15:40:31 -0500

    Add class to propagate the type of ErrorRecorder being used throughout the application. Necessary because of various places in the code calling new ErrorRecorders not tied into the ErrRec initialization.

---
+++ error-recording/src/main/java/gov/nist/toolkit/errorrecording/SelectedErrorRecorder.java
@@ -0,0 +1,33 @@
+package gov.nist.toolkit.errorrecording;
+
+/**
+ * Created by diane on 3/9/2017.
+ * Indicates the type of the current ErrorRecorder (GWT or XML). For temporary use while refactoring the code. Singleton.
+ */
+
+public class SelectedErrorRecorder {
+    public enum ErrorRecorderType {UNDEFINED, GWT_ERROR_RECORDER, XML_ERROR_RECORDER};
+    SelectedErrorRecorder selectedErrorRecorder = null;
+    ErrorRecorderType type = ErrorRecorderType.UNDEFINED;
+
+
+    private SelectedErrorRecorder() {
+        selectedErrorRecorder = this;
+    }
+
+        public SelectedErrorRecorder getSelectedErrorRecorder(){
+        if (selectedErrorRecorder != null) {
+            return selectedErrorRecorder;
+        }
+        return new SelectedErrorRecorder();
+    }
+
+    public void setSelectedErrorRecorder(ErrorRecorderType _type){
+        type = _type;
+    }
+
+    public ErrorRecorderType getType(){
+        return type;
+    }
+
+}
