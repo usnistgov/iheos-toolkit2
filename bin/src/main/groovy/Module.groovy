909974bff00e9cc534ad7ee96b7a049f7c81ebcb
Bill Majurski <bmajur@gmail.com>
2017-06-28 10:13:38 -0400

    Adding scripts for breakup

--- bin/src/main/groovy/Module.groovy
+++ bin/src/main/groovy/Module.groovy
@@ -5,6 +5,52 @@
 class Module {
     static ROOT = new File('/Users/bill/tk')
 
+    static List<File> calledBy(String moduleName) {
+        String pkgName = packageName(asModule(moduleName))
+        println "${moduleName} ==> ${pkgName}"
+        List<File> sourceFiles = getSourceFiles(moduleName)
+        Set calledBy = []
+        sourceFiles.each {File f ->
+            f.eachLine { String line ->
+                if (line.indexOf('import') > -1 && line.index(pkgName) > -1)
+                    calledBy.add(f.name)
+            }
+        }
+        calledBy
+    }
+
+    static File asModule(String moduleName) {
+        new File(ROOT, moduleName)
+    }
+
+    static String packageName(File module) {
+        File main = new File(new File(module, 'src'), 'main')
+        if (!main.exists()) return null
+        File groovy = new File(main, 'groovy')
+        println "groovy: ${groovy}"
+        if (groovy.exists() && groovy.listFiles().size() > 0) {
+            // TODO - dig into the source tree
+            File firstFile = groovy.listFiles()[0]
+            String[] parts = firstFile.name.split('.')
+            if (parts.size() >= 4) {
+                def pkg = parts[3]
+                return pkg
+            }
+        }
+        File jav = new File(main, 'java')
+        println "jav: ${jav}"
+        if (jav.exists() && jav.listFiles().size() > 0) {
+            // TODO - dig into the source tree
+            File firstFile = jav.listFiles()[0]
+            String[] parts = firstFile.name.split('.')
+            if (parts.size() >= 4) {
+                def pkg = parts[3]
+                return pkg
+            }
+        }
+        return null
+    }
+
     static List<File> getModules() {
         ROOT.listFiles().findAll() { File module ->
             new File(module,'pom.xml').exists()
@@ -17,7 +63,6 @@ class Module {
 
     static List<File> getSourceFiles(File module) {
         def main = new File(new File(module, 'src'), 'main')
-        println main
         findSrc(main, ['java', 'groovy'])
     }
 
@@ -32,14 +77,15 @@ class Module {
         src
     }
 
-    static fileExtension(File file) {
-        def pointIndex = file.name.lastIndexOf('\\.')
-        if (pointIndex != -1)
+    static String fileExtension(File file) {
+        def pointIndex = file.name.lastIndexOf('.')
+        if (pointIndex != -1) {
             return file.name.substring(pointIndex + 1)
+        }
         return null
     }
 
-    static boolean isTest(File file) { file.name.indexOf('Test\\.') == -1 }
+    static boolean isTest(File file) { file.name.indexOf('Test.') != -1 }
 
     static main(def argv) {
         println getSourceFiles('fhir')
