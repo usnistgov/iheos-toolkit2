838e7114698490af4f9ded6264de5ec016806212
Sunil Bhaskarla <skbhaskarla@gmail.com>
2018-05-14 16:11:32 -0400

    Configure https for UI in web.xml, which is OFF by default, and add new httpUI profile option. There is a new web.xml with the security markup. Alternate web.xml implies dual source maintenance. Add a new httpsUI script parameter and update the build release script.

--- build-release.sh
+++ build-release.sh
@@ -5,9 +5,10 @@
 # Do the whole build including installing the toolkit.properties file
 
 function usage() {
-	echo "Usage: $SCRIPTNAME configuration-name"
+	echo "Usage: $SCRIPTNAME configuration-name [httpsUI=on|off]"
 	echo "Where configuration-name is a directory under properties/"
 	echo "that holds a toolkit.properties file to incorporate."
+	echo "httpsUI uses a profile activated web.xml configured to redirect http UI requests to https."
 }
 
 
@@ -22,13 +23,27 @@ fi
 PROPERTIESNAME=$1
 PROPERTIESFILE=$BUILDDIR/properties/$PROPERTIESNAME/toolkit.properties
 
+HTTPSUI=$2
+
 if [ ! -f $PROPERTIESFILE ]
 then
 	echo "Properties file $PROPERTIESFILE does not exist"
 	exit -1
 fi
 
-mvn clean install -P Bill -D skipTests
+if [ ! -z $HTTPSUI ]
+then
+    if [ $HTTPSUI = "on" ]
+    then
+        echo "httpsUI option is on."
+#	    Note: If HTTPS UI web.xml configuration is desired you must use an additional -PhttpsUI maven profile.
+        mvn clean install -P Bill -PhttpsUI -D skipTests
+    else
+        mvn clean install -P Bill -D skipTests
+    fi
+else
+    mvn clean install -P Bill -D skipTests
+fi
 
 bash pre-release.sh
 
