1d0582b187d8e14c0f0eb074b7689929ece67452
Sunil <skbhaskarla@gmail.com>
2019-02-20 20:32:02 -0500

    In the event Toolkit started with an invalid external cache update, automatically initialize FHIR support server after the external cache is updated through the Toolkit Configuration UI tool.

--- toolkit-services/src/main/java/gov/nist/toolkit/toolkitServices/httpApi/ToolkitServicesStartup.groovy
+++ toolkit-services/src/main/java/gov/nist/toolkit/toolkitServices/httpApi/ToolkitServicesStartup.groovy
@@ -10,7 +10,10 @@ import groovy.transform.TypeChecked
 import org.apache.log4j.Logger
 
 import javax.servlet.ServletConfig
+import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * wait for the servlets to all startup up and then make sure FHIR Support Server is running.
@@ -18,6 +21,7 @@ import javax.servlet.http.HttpServlet
 @TypeChecked
 class ToolkitServicesStartup extends HttpServlet {
     static Logger logger = Logger.getLogger(ToolkitServicesStartup.class);
+    static boolean isDeferred = false
 
     class Init extends Thread {
 
@@ -28,12 +32,22 @@ class ToolkitServicesStartup extends HttpServlet {
                     FhirSupportOrchestrationRequest fRequest = new FhirSupportOrchestrationRequest()
                     fRequest.useExistingState = true
                     fRequest.testSession = new TestSession('default')
-                    Session session = new Session(Installation.instance().warHome())
-                    session.setEnvironment('default')
-
-                    FhirSupportOrchestrationResponse fResponse = (FhirSupportOrchestrationResponse) new OrchestrationManager().buildFhirSupportEnvironment(session, fRequest)
-                    String isNot = (!fResponse.isError()) ? '' : 'not'
-                    logger.info("FHIR Support Server was ${isNot} started")
+                    File warHome = Installation.instance().warHome()
+                    Session session = new Session(warHome)
+                    String startStatus = ""
+                    if (!Installation.instance().externalCache()) {
+                        isDeferred = true
+                       startStatus =
+                               "deferred due to an invalid external cache location. FHIR Server will be initialized when the external cache is fixed either through:\n" \
+                                + "a) the Toolkit Configuration update UI tool (restart is not necessary)\n" \
+                                + "b) the toolkit.property file update (restart is necessary)."
+                    } else {
+                        isDeferred = false
+                        session.setEnvironment('default')
+                        FhirSupportOrchestrationResponse fResponse = (FhirSupportOrchestrationResponse) new OrchestrationManager().buildFhirSupportEnvironment(session, fRequest)
+                        startStatus = (!fResponse.isError()) ? 'started' : 'not started'
+                    }
+                    logger.info("FHIR Support Server was ${startStatus}")
                 }  catch (Throwable e) {
                     logger.fatal("Failed to launch FHIR Support Server: ${e.getMessage()}")
                     logger.fatal("${e}")
@@ -47,4 +61,28 @@ class ToolkitServicesStartup extends HttpServlet {
         new Init().start()
     }
 
+    @Override
+    void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String cmd = request.getParameter("cmd")
+            if (cmd!=null) {
+                if ("isDeferred".equals(cmd)) {
+                    response.setContentType("text/plain")
+                    response.setStatus(HttpServletResponse.SC_OK)
+                    PrintWriter writer = response.getWriter()
+                    writer.print(isDeferred)
+                    writer.close()
+                } else if ("start".equals(cmd)) {
+                    if (isDeferred) {
+                        response.setStatus(HttpServletResponse.SC_OK)
+                       new Init().start()
+                    } else {
+                        response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED)
+                    }
+                    return
+                } else {
+                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    return
+                }
+            }
+    }
 }
