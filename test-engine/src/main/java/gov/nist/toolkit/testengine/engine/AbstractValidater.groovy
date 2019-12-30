b34d5c60436284e3924925495ad1f82b64a3c882
bill <bmajur@gmail.com>
2019-12-30 09:02:17 -0500

    Fixes #503 - support methods had been removed. Test XdsDocSrcSubmissionTest-1111/SubmitSingleDoc now passes.

--- test-engine/src/main/java/gov/nist/toolkit/testengine/engine/AbstractValidater.groovy
+++ test-engine/src/main/java/gov/nist/toolkit/testengine/engine/AbstractValidater.groovy
@@ -1,11 +1,40 @@
 package gov.nist.toolkit.testengine.engine
 
+import gov.nist.toolkit.testengine.engine.validations.ValidaterResult
 
 abstract class AbstractValidater<T> {
     String filterDescription
+    StringBuilder log = new StringBuilder()
+    boolean errors = false
+
+    abstract ValidaterResult validate(T transactionInstance)
+
+    AbstractValidater<T> reset() {
+        log = new StringBuilder()
+        errors = false
+        this
+    }
+
+    void log(String msg) {
+        log.append(msg).append('\n')
+    }
+
+    void error(String msg) {
+        log("Error: ${msg}")
+        errors = true
+    }
+
+    void error(String type, String msg) {
+        log("Error: ${type}:  ${msg}")
+        errors = true
+    }
+
     void setFilterDescription(String filterDescription) {
         this.filterDescription = filterDescription
     }
 
+    String getLog() {
+        log.toString()
+    }
 
 }
