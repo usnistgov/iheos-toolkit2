5b6ed751fc5065104ceb6da46003a0c3e58872ee
Bhaskarla <skb1@NIST.GOV>
2021-12-29 16:26:17 -0500

    Fixes assertion plugin error propagation #535
    Following is an example of log messages that can now be seen from the UI
    Runs an ExpectedContent validater (Validator#run_test_assertions) through this plugin.
    Error: Request:
    Validator: Found 1 Documents instead of 2

--- test-engine/src/main/java/gov/nist/toolkit/testengine/engine/validations/ValidaterResult.groovy
+++ test-engine/src/main/java/gov/nist/toolkit/testengine/engine/validations/ValidaterResult.groovy
@@ -16,12 +16,17 @@ class ValidaterResult {
      */
     def filter
     boolean match
-    private StringBuilder log = new StringBuilder()
+    private StringBuilder log
 
     ValidaterResult(def transaction, /* PostValidater */ AbstractValidater filter, boolean match) {
         this.transaction = transaction
         this.filter = filter
         this.match = match
+        if (filter.log) {
+            this.log = new StringBuilder(filter.log)
+        } else {
+            this.log = new StringBuilder()
+        }
     }
 
     def log(String msg) {
