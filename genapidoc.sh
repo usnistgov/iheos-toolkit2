785a0980c72d78b83244cbf8398c3b3e9cdf0263
Sunil <skbhaskarla@gmail.com>
2024-03-30 11:55:01 -0400

    Add classpath to javadoc to fix error
    (cherry picked from commit f437a4d732a5d7e5c5d579fb0edbe3636b15160e)

--- genapidoc.sh
+++ genapidoc.sh
@@ -3,6 +3,9 @@
 #groovydoc --destdir ~/tmp/toolkit-documentation gov.nist.toolkit.toolkitApi toolkit-api/src/main/java/gov/nist/toolkit/toolkitApi/*.java
 
 output=$1
+# usually needs the special convenience path/* asterisk at the end, to include all jar files (javadoc feature)
+cp=$2
+
 echo "Writing output to $output"
 #rm -rf $output
 mkdir -p $output
@@ -10,7 +13,8 @@ mkdir -p $output
 echo -n "Running javadoc from "
 pwd
 
-javadoc -d $output \
+# -sourcepath ~/myprojects/iheos-toolkit2/toolkit-api/src
+javadoc -source 8  -classpath "$cp" -d $output \
 `find toolkit-api -name '*.java'  -not -name '*XdsDocumentRegRep.java' -not -name '*XcaInitiatingGateway.java' -not -name '*XcaRespondingGateway.java' -not -name '*XdsDocumentConsumer.java' -not -name '*EngineSpi.java' -not -name '*BasicSimParameters.java'` \
 `find toolkit-services-common -name '*.java' -not -name '*Resource.java' -not -name '*Adapter.java'` \
 `find toolkit-services-common -name DocumentResource` \
